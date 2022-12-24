357ffe8f9c989763eab9a8814cfdca9c9dcec70a
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-24 08:57:46 +0100

    work

--- src/main/java/org/woehlke/computer/kurzweil/wator/WatorApplication.java
+++ src/main/java/org/woehlke/computer/kurzweil/wator/WatorApplication.java
@@ -12,7 +12,6 @@ import org.woehlke.computer.kurzweil.wator.view.WatorFrame;
  *
  * @see WatorFrame
  *
- * @see <a href="https://thomas-woehlke.blogspot.com/2016/01/mandelbrot-set-drawn-by-turing-machine.html">Blog Article</a>
  * @see <a href="https://github.com/Computer-Kurzweil/simulated-evolution">Github Repository</a>
  * @see <a href="https://java.woehlke.org/simulated-evolution/">Maven Project Repository</a>
  */
